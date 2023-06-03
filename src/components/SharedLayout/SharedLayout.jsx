6a5dfa582f77d38f70038ef5dcf2aab98c14d893
Myra77 <112496647+Myra77@users.noreply.github.com>
2023-06-04 01:13:16 +0200

    sharedlayout_update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,21 @@
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
-import css from './SharedLayout.module.css';
+import {Header} from './SharedLayout.styled';
 
 const SharedLayout = () => {
     return (
         <div>
-            <ul className={css['Header']}>
-                <li>
+            <Header>
+            <li>
                     <NavLink to="/">Home</NavLink>
                 </li>
                 <li>
                     <NavLink to="/movies">Movies</NavLink>
                 </li>
-            </ul>
+            </Header>
+                
+            
             <main>
                 <Suspense fallback={<div>Loading...</div>}>
                 <Outlet />
